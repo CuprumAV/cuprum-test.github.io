2bb1cc4ad23cef450a0399ee9579996f69e1ae65
CuprumAV <78966554+CuprumAV@users.noreply.github.com>
2021-03-06 22:44:36 +0200

    Add files via upload

---
+++ cu_test/js/script.js
@@ -0,0 +1,8 @@
+var cuprum = document.getElementById("cuprum");
+var osn = document.getElementById("osn");
+function cuprum_funct() {
+    alert("YOU TAP");
+    document.getElementById("cuprum").style.color = "var(--c-panel)";
+    document.getElementById("cuprum").style.transform = "scale(1.10)";
+    document.getElementById("cuprum").style.margin="70px 0 0 -10px";
+}
